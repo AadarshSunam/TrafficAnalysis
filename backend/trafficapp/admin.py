3617e46a1e6517741f3f919e34dce77e726fbe2b
Aadarsha Sunam Pariyar <aadarshsunam4@gmail.com>
2025-06-05 18:44:57 +0545

    Define models and register in admin; apply migrations and create superuser.

--- backend/trafficapp/admin.py
+++ backend/trafficapp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Scenario, VideoSource, Intersection, VehicleClass, VehicleCount
 
-# Register your models here.
+@admin.register(Scenario)
+class ScenarioAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(VideoSource)
+class VideoSourceAdmin(admin.ModelAdmin):
+    list_display = ("name", "path", "scenario")
+
+@admin.register(Intersection)
+class IntersectionAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(VehicleClass)
+class VehicleClassAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(VehicleCount)
+class VehicleCountAdmin(admin.ModelAdmin):
+    list_display = ("intersection", "vehicle_class", "direction", "timestamp", "count", "scenario")
+    list_filter = ("scenario", "intersection", "vehicle_class", "direction")
+    ordering = ("-timestamp",)
